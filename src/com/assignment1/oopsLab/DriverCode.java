46bd3dd7bd4c9bfe357e2c002f2665ed9392dae5
Aakash Gupta <aakashgupta7355@gmail.com>
2021-12-31 21:00:12 +0530

    initial commit 5

---
+++ src/com/assignment1/oopsLab/DriverCode.java
@@ -0,0 +1,36 @@
+package com.assignment1.oopsLab;
+
+public class DriverCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AdminDepartment ad = new AdminDepartment(); // OBJECT CREATION
+		ad.departmantName();
+		ad.getTodaysWork();
+		ad.getWorkDeadline(); // METHOD OVERLOADING
+		holiday(); // METHOD OVERRIDING
+		System.out.println("---------------------------");
+		
+		HrDepartment hd = new HrDepartment();
+		hd.departmantName();
+		hd.getTodaysWork(); // DISPLAYING METHOD FUNCTIONALITIES BY USING ITS OBJECT
+		hd.getWorkDeadline();
+		hd.doActivity();
+		holiday();
+		System.out.println("---------------------------");
+		
+		TechnicalDepartment td = new TechnicalDepartment();
+		td.departmantName();
+		td.getTodaysWork();
+		td.getWorkDeadline();
+		td.getTechStachInformation();
+		holiday();
+		System.out.println("---------------------------");
+	}
+
+	static void holiday() {
+		System.out.println("Today is not a holiday"); // DISPLAYIMG "TODAY IS NOT A HOLIDAY WITHOUT THE HELP OF SUPER
+														// CLASS
+	}
+}
